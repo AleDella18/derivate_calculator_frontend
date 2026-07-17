bbf7279b90087cbcf98fa080028a45cd7c4e96c4
AleDella18 <aledella18@gmail.com>
2026-07-17 10:21:24 +0200

    fix: modify SECRET_KEY

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
-const SECRET_KEY = process.env.SECRET_KEY || ";+z8X*(cmbN|#si#";
+const SECRET_KEY = process.env.SECRET_KEY || "BrUJTuTS28idUj5sfo2370BkUREjY3M2CJjp01UVrNm";
 
 async function isTokenValid(token: string): Promise<boolean> {
   try {
@@ -13,23 +13,29 @@ async function isTokenValid(token: string): Promise<boolean> {
     });
 
     return true;
-  } catch {
+  } catch (error: any) {
+    console.error(error);
     return false;
   }
 }
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("auth_token")?.value;
+  
   const { pathname } = request.nextUrl;
+  
 
   const loginUrl = new URL("/login", request.url);
 
   if (pathname === "/login") {
+    
     if (!token) {
       return NextResponse.next();
     }
 
     const valid = await isTokenValid(token);
+    
+
     if (!valid) {
       const response = NextResponse.next();
       response.cookies.delete("auth_token");
@@ -40,11 +46,14 @@ export async function middleware(request: NextRequest) {
   }
 
   if (pathname === "/") {
+    
     if (!token) {
       return NextResponse.redirect(loginUrl);
     }
 
     const valid = await isTokenValid(token);
+    
+
     if (!valid) {
       const response = NextResponse.redirect(loginUrl);
       response.cookies.delete("auth_token");
